f7a1a1590e9e27bc4a8416f961b7cc6fd8188e7d
Samantha Pelková <102859715+SamanthaPelkova@users.noreply.github.com>
2023-07-13 10:21:13 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import image1 from "./images/Iphone-14-Pro-Max.png"
+import image2 from "./images/Iphone-14-Pro.png"
+import image3 from "./images/Iphone-14.png"
+
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Iphone 14 Pro Max",
+      price: 1999,
+      tag: "512 Gb",
+      image: image1,
+    },
+    {
+      id: "2",
+      name: "Iphone 14 Pro",
+      price: 899,
+      tag:"256 Gb",
+      image: image2,
+    },
+    {
+      id: "3",
+      name: "Iphone 14",
+      price: 999,
+      tag: "128 Gb",
+      image: image3,
+    },
+  ],
+};
+export default data;
